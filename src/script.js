d9766ea666125aedeea207e16164f4b77563d209
William Almeida <willliam.dev@gmail.com>
2023-06-17 19:40:16 -0300

    add event listeners to run the app

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
-// script
+
+const binaryInput = document.querySelector("#input-number")
+const button = document.querySelector("#convert")
+const modal = document.querySelector("dialog")
+const result = document.querySelector("#result")
+
 function numberHandler(binaryNumber, modal) {
   let binaryArray = []
+
   for (let index = 0; index < binaryNumber.length; index++) {
-
     const digit = Number(binaryNumber[index])
-
     if (digit === 0 || digit === 1) {
       binaryArray.push(digit)
     } else {
       modal.showModal()
-      return
+      return null
     }
   }
 
@@ -30,15 +34,24 @@ function parseBinarytoDecimal(binary) {
   return decimal
 }
 
-function app() {
-  const modal = document.querySelector("dialog")
-  const binary = document.querySelector("#input-number").value
-  const result = document.querySelector("#result")
-
-  const binaryNumber = numberHandler(binary, modal)
+function convert() {
+  const binaryNumber = numberHandler(binaryInput.value, modal)
   const decimalNumber = parseBinarytoDecimal(binaryNumber)
 
   if (decimalNumber) {
     result.innerText = `Decimal: ${decimalNumber}`
+  } else {
+    result.innerText = "" // clear
   }
 }
+
+
+button.addEventListener("click", () => {
+  convert()
+})
+
+binaryInput.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    convert()
+  }
+})
